8ece10f901b0ba297ca8311f06665e4c99896180
Eduardo Santos Barreto <eduardopontobarreto@gmail.com>
2021-09-05 20:01:52 -0300

    erro 500 ao passar parametros corrigido

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, request
 import os
+from flask import Flask, request
 
 from homeworker import get_summary
 
@@ -8,44 +8,56 @@ app = Flask('Homeworker')
 
 @app.route('/getSummary', methods=['POST'])
 def return_summary():
+    if request.method == 'POST':
 
-    body = request.get_json()
+        body = request.get_json()
 
-    parameters = {
-        'topic': 'Sponge Bob', 'lang_to_search': 'en',
-        'num_sentences': 6, 'num_references': 3
-    }
+        parameters = {
+            'topic': 'Sponge Bob', 'lang_to_search': 'en',
+            'num_sentences': 6, 'num_references': 3
+        }
 
-    if 'topic' not in body:
-        return set_response(400, 'The parameter "topic" is mandatory')
-
-    parameters['topic'] = body['topic']
-
-    optional_parameters = ['lang_to_search', 'num_sentences', 'num_references']
-    for param in optional_parameters:
-        if param in body:
-            parameters[param] = body[param]
-
-    if parameters['lang_to_search'] == 'en':
         try:
-            response = get_summary(**parameters)
+            if 'topic' not in body:
+                return set_response(400, 'The parameter "topic" is mandatory')
         except:
+            body = request.form.to_dict()
+            if 'topic' not in body:
+                return set_response(400, 'The parameter "topic" is mandatory')
+
+        parameters['topic'] = body['topic']
+
+        optional_parameters = ['lang_to_search',
+                               'num_sentences', 'num_references']
+        for param in optional_parameters:
+            if param in body:
+                if param == 'lang_to_search':
+                    parameters[param] = body[param]
+                else:
+                    parameters[param] = int(body[param])
+
+        if parameters['lang_to_search'] == 'en':
             try:
-                parameters['lang_to_search'] = 'es'
                 response = get_summary(**parameters)
             except:
-                return set_response(404, 'Content not found.')
+                try:
+                    parameters['lang_to_search'] = 'es'
+                    response = get_summary(**parameters)
+                except:
+                    return set_response(404, 'Content not found.')
+        else:
+            try:
+                response = get_summary(**parameters)
+            except:
+                try:
+                    parameters['lang_to_search'] = 'en'
+                    response = get_summary(**parameters)
+                except:
+                    return set_response(404, 'Content not found.')
+
+        return set_response(200, 'Successfully generated summary.', nome_do_conteudo='content', conteudo=response)
     else:
-        try:
-            response = get_summary(**parameters)
-        except:
-            try:
-                parameters['lang_to_search'] = 'en'
-                response = get_summary(**parameters)
-            except:
-                return set_response(404, 'Content not found.')
-
-    return set_response(200, 'Successfully generated summary.', nome_do_conteudo='content', conteudo=response)
+        return set_response(400, 'Unsupported request method')
 
 
 def set_response(status, mensagem, nome_do_conteudo=False, conteudo=False):
@@ -62,7 +74,6 @@ def set_response(status, mensagem, nome_do_conteudo=False, conteudo=False):
 def main():
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-    app.run()
 
 
 if __name__ == '__main__':
